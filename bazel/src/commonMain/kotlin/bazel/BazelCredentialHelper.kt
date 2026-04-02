dd5b4d9fc004cb97a80de19f5bd5258516c43cca
Oliver Heger <oliver.heger@bosch.com>
2026-04-02 11:24:18 +0200

    feat(bazel): Implement the Bazel credential helper
    Complete the implementation analogously to the one for Git. Also add a
    test class.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.com>

--- bazel/src/commonMain/kotlin/bazel/BazelCredentialHelper.kt
+++ bazel/src/commonMain/kotlin/bazel/BazelCredentialHelper.kt
@@ -19,6 +19,90 @@
 
 package org.eclipse.apoapsis.ortserver.credentialhelper.bazel
 
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.okio.decodeFromBufferedSource
+
+import okio.BufferedSource
+
+import org.eclipse.apoapsis.ortserver.credentialhelper.common.AuthenticationInfo
+import org.eclipse.apoapsis.ortserver.credentialhelper.common.CredentialHelperResult
+import org.eclipse.apoapsis.ortserver.credentialhelper.common.CredentialsRequest
+import org.eclipse.apoapsis.ortserver.credentialhelper.common.RequestUrl
+import org.eclipse.apoapsis.ortserver.credentialhelper.common.credentialHelperMain
+import org.eclipse.apoapsis.ortserver.credentialhelper.common.getHomeDirectory
+
+/** The name of the command that triggers a request for credentials. */
+private const val BAZEL_GET_CREDENTIALS_COMMAND = "get"
+
+/** The name of the file containing the credentials for Bazel. */
+private const val BAZEL_CREDENTIALS_FILE_NAME = ".bazel-credentials"
+
+/** The JSON instance used to serialize and deserialize Bazel credential helper messages. */
+internal val json = Json { ignoreUnknownKeys = true }
+
+/**
+ * Generate credentials for Bazel based on the input provided via stdin as JSON and the Bazel credentials file.
+ *
+ * The expected input format is a JSON object with a `uri` field:
+ * ```json
+ * { "uri": "https://registry.example.com/some/path" }
+ * ```
+ *
+ * If credentials are found for the given URI, the output is a JSON object containing an `Authorization` header
+ * with a Basic Auth value:
+ * ```json
+ * { "headers": { "Authorization": ["Basic <base64(user:password)>"] } }
+ * ```
+ *
+ * If no credentials are found, an empty headers map is returned, but the exit code is still 0, since the request was
+ * handled completely.
+ *
+ * See https://github.com/EngFlow/credential-helper-spec
+ */
 fun main(args: Array<String>) {
-    // TODO: Implement the Bazel credential helper.
+    val request = CredentialsRequest(
+        inputReader = ::parseInputSource,
+        resultGenerator = { _, authInfo -> generateResult(authInfo) },
+        credentialsFile = getHomeDirectory().resolve(BAZEL_CREDENTIALS_FILE_NAME),
+        credentialCommand = BAZEL_GET_CREDENTIALS_COMMAND
+    )
+
+    credentialHelperMain(args, request)
 }
+
+/**
+ * Parse the given [inputSource] (JSON) to extract a [RequestUrl] with the URI from the [GetCredentialRequest].
+ */
+@OptIn(ExperimentalSerializationApi::class)
+private fun parseInputSource(inputSource: BufferedSource): Result<RequestUrl<Unit>> =
+    runCatching {
+        val credentialRequest = json.decodeFromBufferedSource<GetCredentialRequest>(inputSource)
+        RequestUrl(url = credentialRequest.uri, context = Unit)
+    }
+
+/**
+ * Generate the result for the given [authInfo]. If credentials have been found for the current request, the response
+ * contains a Basic Auth `Authorization` header. Otherwise, no headers are generated.
+ */
+private fun generateResult(authInfo: AuthenticationInfo?): CredentialHelperResult {
+    val response = authInfo?.let { info ->
+        val basicToken = encodeBasicAuth(info.username, info.password)
+        GetCredentialResponse(headers = mapOf("Authorization" to listOf("Basic $basicToken")))
+    } ?: GetCredentialResponse()
+
+    return CredentialHelperResult(
+        output = json.encodeToString(GetCredentialResponse.serializer(), response),
+        exitCode = 0
+    )
+}
+
+/**
+ * Encode the given [username] and [password] as a Base64-encoded Basic Auth token.
+ */
+@OptIn(ExperimentalEncodingApi::class)
+internal fun encodeBasicAuth(username: String, password: String): String =
+    Base64.encode("$username:$password".encodeToByteArray())
